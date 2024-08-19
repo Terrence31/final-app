be64b8499ec461a10b004850eed555a2a7591d36
mathiasferns <113444715+mathiasferns@users.noreply.github.com>
2024-08-19 08:04:49 +0000

    bro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold text-center my-8">Your Main App Content</h1>
+      <div className="langflow">
+      <langflow-chat
+          window_title="Chatbot_cohere"
+          flow_id="697fb9c8-9f17-46e2-b61d-819c5dd2be61"
+          host_url="http://localhost:7860"
+          chat_position="top-left"          
+          height={500}
+          width={300}
+          chat_trigger_style='{ "background-size": "cover", "background-position": "center","backgroundColor":"yellow", "color":"black"}'
+          chat_window_style='{"border": "2px solid black"}'  
+        ></langflow-chat>
+        </div>
     </div>
   );
 }
